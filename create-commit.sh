b616e6f700cc88e5acc8ee5ab77077e0014d7a85
Beknazar0707 <102185863+Beknazar0707@users.noreply.github.com>
2024-04-04 11:52:22 +0600

    Squ (#6)
    * First file commit
    
    * Second file commit
    
    * Changes made 1 at четверг,  4 апреля 2024 г. 11:40:54 (+06)
    
    Changes made 2 at четверг,  4 апреля 2024 г. 11:40:54 (+06)
    
    Changes made 3 at четверг,  4 апреля 2024 г. 11:40:54 (+06)
    
    Changes made 4 at четверг,  4 апреля 2024 г. 11:40:54 (+06)
    
    Changes made 5 at четверг,  4 апреля 2024 г. 11:40:54 (+06)
    
    Changes made 6 at четверг,  4 апреля 2024 г. 11:40:54 (+06)
    
    Changes made 7 at четверг,  4 апреля 2024 г. 11:40:54 (+06)
    
    Changes made 8 at четверг,  4 апреля 2024 г. 11:40:54 (+06)
    
    Changes made 9 at четверг,  4 апреля 2024 г. 11:40:54 (+06)
    
    Changes made 10 at четверг,  4 апреля 2024 г. 11:40:54 (+06)
    
    * Changes made 1 at четверг,  4 апреля 2024 г. 11:46:59 (+06)
    
    * Changes made 2 at четверг,  4 апреля 2024 г. 11:46:59 (+06)
    
    * Changes made 3 at четверг,  4 апреля 2024 г. 11:46:59 (+06)
    
    * Changes made 4 at четверг,  4 апреля 2024 г. 11:46:59 (+06)
    
    * Changes made 5 at четверг,  4 апреля 2024 г. 11:46:59 (+06)
    
    * Changes made 6 at четверг,  4 апреля 2024 г. 11:46:59 (+06)
    
    * Changes made 7 at четверг,  4 апреля 2024 г. 11:46:59 (+06)
    
    * Changes made 8 at четверг,  4 апреля 2024 г. 11:46:59 (+06)
    
    * Changes made 9 at четверг,  4 апреля 2024 г. 11:46:59 (+06)
    
    * Changes made 10 at четверг,  4 апреля 2024 г. 11:46:59 (+06)
    
    ---------
    
    Co-authored-by: beknazar007 <saitovbeknazar6@gmail.com>

--- create-commit.sh
+++ create-commit.sh
@@ -9,20 +9,20 @@ new_branch=$1
 
 # Create 5 files and make some changes to them
 touch "file-$new_branch.txt"
-git checkout -b "$new_branch" main
+# git checkout -b "$new_branch" main
 git checkout  "$new_branch" 
-for i in {1..5}
+for i in {1..10}
 do
     
-    echo "This is file $i" >> "file-$new_branch.txt"
+    echo "This is file $i" >> "file.txt"
     # Stage all changes
     git add .
     # Commit the changes with a timestamp
-    commit_message="Changes made at $(date)"
+    commit_message="Changes made $i at $(date)"
     git commit -m "$commit_message"
 done
 
 
 # Push the changes to the new branch
-git push origin "$new_branch"
+# git push origin "$new_branch"
 echo "Changes committed and pushed to branch '$new_branch'"
